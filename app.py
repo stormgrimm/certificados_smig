c4b5bd9a1aa25733519f9c60a8d75105627c1a3d
Luis Armando Ramírez López <luisrmzl@proton.me>
2025-06-05 21:31:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if uploaded_file:
             for nombre in df["Nombre"]:
                 body += rf"""
 \AddToShipoutPictureBG*{{\includegraphics[width=\paperwidth,height=\paperheight]{{plantilla.pdf}}}}
-\vspace*{{8.5cm}}  % Ajustar conforme a la plantilla
+\vspace*{{6cm}}  % Ajustar conforme a la plantilla
 \begin{{center}}
     \hspace*{{-3cm}}
     \color{{verdeSMIG}}
